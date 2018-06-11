e45d2bb287c70a6df059870137b3d6dba2b3304a
renrizzolo <renrizmusic@gmail.com>
2018-06-11 20:57:38 +1000

    add Tag route component (allPosts with filter for single tag)

---
+++ src/views/Tag.js
@@ -0,0 +1,69 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Query } from 'react-apollo';
+import Panel from '../components/Panel';
+import Container from '../components/Container';
+
+import Loading from '../components/Loading';
+import Error from '../components/Error';
+import { allPosts } from '../queries';
+
+const POSTS_PER_PAGE = 4;
+
+const tagQueryVars = {
+  skip: 0,
+  first: POSTS_PER_PAGE,
+};
+
+const Tag = ({match}) => (
+  <Container>
+    <section>
+      <Query query={allPosts} variables={{ ...tagQueryVars, filter: { tags_every: { tag: match.params.tag } }, notifyOnNetworkStatusChange: true }}>
+      {({
+        loading, error, data: { allPosts, _allPostsMeta }, networkStatus 
+        }, loadMorePosts) => {
+      console.log(loading, error, allPosts);
+        if (loading) return <Loading />;
+        if (error) return <Error>Couldn't load the post</Error>;
+      const areMorePosts = _allPostsMeta.count > allPosts.length;
+      return (
+        <React.Fragment>
+          <ul>
+            {allPosts.length ?
+              allPosts.map(post => (
+                <li key={`post-${post.id}`}>
+                  <Link to={`/post/${post.slug}`}>
+                    <div>
+                      <img
+                        alt={post.title}
+                        src={`https://media.graphcms.com/resize=w:100,h:100,fit:crop/${post.coverImage.handle}`}
+                      />
+                    </div>
+                    <h3>{post.title}</h3>
+                  </Link>
+                </li>
+
+            ))
+              : 
+              <Panel>
+                <h3>If I made a post, it'd be here.</h3>
+              </Panel>
+              }
+          </ul>
+          <div className="Home-showMoreWrapper">
+            {areMorePosts
+              ? <button className="Home-button" disabled={loading} onClick={() => loadMorePosts()}>
+                {loading ? 'Loading...' : 'Show More Posts'}
+              </button>
+              : ''}
+          </div>
+        </React.Fragment>
+        );
+      }}
+      </Query>
+    </section>
+  </Container>
+);
+
+
+export default Tag;
