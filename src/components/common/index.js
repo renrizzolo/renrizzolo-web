38b44f247f8a3e21fa3f8c676c17e098fc487ecb
renrizzolo <renrizmusic@gmail.com>
2018-06-11 20:56:24 +1000

    fix post heading styles

--- src/components/common/index.js
+++ src/components/common/index.js
@@ -8,7 +8,9 @@ export const Heading = styled('h1')`
   }
 `;
 export const LargeHeading = styled('h1')`
-  font-size: 4rem;
+  font-size: 3.5rem;
+  line-height: 1.4;
+  margin: 0;
   @media screen and (max-width: 768px) {
     font-size: 2rem;
   }
@@ -18,7 +20,7 @@ export const ImgEl = styled('img')`
     height: auto;
 `;
 export const Img = (props) => (
-    <figure itemprop="image" itemScope ="" itemtype ="https://schema.org/ImageObject">
+    <figure itemprop="image" itemscope ="" itemtype ="https://schema.org/ImageObject">
         <ImgEl {...props}/>
     </figure>
 )
